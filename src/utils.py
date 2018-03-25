2ebae08c733ab85eca643c742c7eb45c42bf11aa
YasenPetrov <yasenpetr@gmail.com>
2018-03-25 15:46:55 +0100

    Fix display function

--- src/utils.py
+++ src/utils.py
@@ -6,16 +6,16 @@ from torch.autograd import Variable
 def show_batch_of_images(img_batch, fig):
     batch_size, im_height, im_width = img_batch.shape
     # calculate no. columns per grid row to give square grid
-    grid_size = int(batch_size**0.5)
+    grid_size = int(np.ceil(batch_size ** 0.5))
     # intialise empty array to tile image grid into
-    tiled = np.empty((im_height * grid_size,
-                      im_width * batch_size // grid_size))
+    tiled = np.zeros((im_height * grid_size,
+                      im_width * grid_size))
     # iterate over images in batch + indexes within batch
     for i, img in enumerate(img_batch):
         # calculate grid row and column indices
-        r, c = i % grid_size, i // grid_size
+        c, r = i % grid_size, i // grid_size
         tiled[r * im_height:(r + 1) * im_height,
-              c * im_height:(c + 1) * im_height] = img
+        c * im_height:(c + 1) * im_height] = img
     ax = fig.add_subplot(111)
     ax.imshow(tiled, cmap='Greys')
     ax.axis('off')
